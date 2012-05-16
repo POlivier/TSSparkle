d04a9de2c05e7356887e0419fad101f282b15283
POlivier <peter.olivier@hotmail.com>
2012-05-16 03:08:40 +0200

    Original

---
+++ js/vouwmenu.js
@@ -0,0 +1,6 @@
+function initMenu() {
+  $('#menu ul').hide();
+  $('#menu li a').click(
+    function() {
+        $(this).next().slideToggle('normal');	});}
+$(document).ready(function() {initMenu();});
